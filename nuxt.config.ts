b73f516f5a8dbb5679fbb43bb5229eaf9ff38542
jeonghun94 <jeongh1021@naver.com>
2023-12-07 10:05:22 +0900

    assets, components, layouts, pages, public 설정

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  css: ["~/assets/scss/global.scss"],
+
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "~/assets/scss/_colors.scss" as *;',
+        },
+      },
+    },
+  },
+  devServer: {
+    port: 3333,
+  },
+});
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Welcome to the homepage</h1>
+    <AppAlert> This is an auto-imported component </AppAlert>
+    <img src="/favicon.ico" alt="favicon" />
+    <img src="~/assets/img/8.png" alt="img" />
+  </div>
+</template>
+<script>
+import "~/assets/css/style.css";
+</script>
